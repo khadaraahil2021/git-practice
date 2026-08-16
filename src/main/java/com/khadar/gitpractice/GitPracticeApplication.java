45e93963e334707e1e9691694bdd3611d58c5bba
KhadarAahil <khadar.ramtulla@gmail.com>
2026-08-16 23:07:50 +0530

    add 3 loggers in practice

--- src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
@@ -8,9 +8,13 @@ public class GitPracticeApplication {
 
     public static void main(String[] args) {
         System.out.println("Add logger in main method");
+
         System.out.println("Add 2 logger in main method");
         System.out.println("Add 3logger in main method");
         System.out.println("Add logger in main method");
+
+        System.out.println("Add logger in main method 2");
+        System.out.println("Add logger in main method 3");
         SpringApplication.run(GitPracticeApplication.class, args);
         System.out.println("Add logger in main method 4");
         System.out.println("Add logger in main method 5");
