3d35a38c9e7f1e092dcc4edd7797acecb578af82
KhadarAahil <khadar.ramtulla@gmail.com>
2026-08-16 22:59:34 +0530

    Add second logger

--- src/main/java/com/khadar/gitpractice/Logger.java
+++ src/main/java/com/khadar/gitpractice/Logger.java
@@ -3,5 +3,6 @@ package com.khadar.gitpractice;
 public class Logger {
     public static void main(String[] args) {
         System.out.println("logger added");
+        System.out.println("2nd logger");
     }
 }
