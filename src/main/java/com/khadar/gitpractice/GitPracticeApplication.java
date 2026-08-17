1a384842bb9ac4d710d6af66329f8273322d73a7
khadaraahil2021 <khadaraahil2021@gmail.com>
2026-08-17 11:17:58 +0530

    8 logger added

--- src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
@@ -15,6 +15,8 @@ public class GitPracticeApplication {
         System.out.println("Add logger in main method 5");
         System.out.println("Add logger in main method 6");
         System.out.println("Add logger in main method 7");
+        System.out.println("Add logger in main method 8");
+
     }
 
 }
