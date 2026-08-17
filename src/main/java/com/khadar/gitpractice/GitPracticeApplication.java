cf66989fb97e1387b68a27a9c3b3cce2c7f8c6f4
khadaraahil2021 <khadaraahil2021@gmail.com>
2026-08-17 11:09:23 +0530

    add 6,7 loggers

--- src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/khadar/gitpractice/GitPracticeApplication.java
@@ -11,6 +11,10 @@ public class GitPracticeApplication {
         System.out.println("Add 2 logger in main method");
         System.out.println("Add 3logger in main method");
         SpringApplication.run(GitPracticeApplication.class, args);
+        System.out.println("Add logger in main method 4");
+        System.out.println("Add logger in main method 5");
+        System.out.println("Add logger in main method 6");
+        System.out.println("Add logger in main method 7");
     }
 
 }
